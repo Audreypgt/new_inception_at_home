8410f4636096aff800d7d3fd0636eb869790b41f
apeuget42 <audrey.peuget@learner.42.tech>
2026-05-23 19:11:44 +0200

    mariadb working git log

--- Makefile
+++ Makefile
@@ -16,13 +16,13 @@ down:
 re:
 	@docker compose -f ./srcs/docker-compose.yaml up -d --build
 
-clean:
-	@docker stop $$(docker ps -qa);\
-	docker compose down;\
-	docker rm $$(docker ps -qa);\
-	docker rmi -f $$(docker images -qa);\
-	docker volume rm $$(docker volume ls -q);\
-	docker network rm $$(docker network ls -q);\
- 	docker system prune -a --volumes;\
+fclean:
+	@cd ./srcs && docker compose down;\
+# 	docker stop $(docker ps -qa);\
+# 	docker rm $(docker ps -qa);\
+# 	docker rmi -f $(docker images -qa);\
+# 	docker volume rm $(docker volume ls -q);\
+# 	docker network rm $(docker network ls -q);\
+ 	docker system prune -a --volumes -f;\
 
 .PHONY: all re down clean
--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-DB_NAME=$(cat /run/secrets/db_name)
-DB_USER=$(cat /run/secrets/db_user)
-DB_PASSWORD=$(cat /run/secrets/db_password)
-DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+# DB_NAME=$(cat /run/secrets/db_name)
+# DB_USER=$(cat /run/secrets/db_user)
+# DB_PASSWORD=$(cat /run/secrets/db_password)
+# DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-mkdir -p run/mysqld
-chown -R mysql:mysql /run/mysqld /var/lib/mysql
+set -x
 
-service mysql start;
+mysqld_safe
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-# modifies root user's password when connected through host machine (local host) as opposed to 'root'@'%', % meaning
+# wait for server to be ready before running mariadb commands
+until mariadb-admin ping --silent; do
+    sleep 1
+done
+
+mariadb << EOF
+CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+FLUSH PRIVILEGES; # apply changes
+EOF
+
+mariadb-admin -u root -p${DB_ROOT_PASSWORD} shutdown # with this line and the next == reboot
+exec mysqld_safe # exec kills the bash script and replaces it by mysql which will take its PID and keep running
+
+# alter user modifies root user's password when connected through host machine (local host) as opposed to 'root'@'%', % meaning
 # everywhere
-mysql -e "FLUSH PRIVILEGES;" # apply changes
-mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown # with this line and the next == reboot
-# OLD: mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown # connect as root
-
-exec mysqld # exec kills the bash script and replaces it by mysql which will take its PID and keep running
-
 # \' \' used to escape database names or tables (useful if it contains special characters) or any of these
 # so-called "identifiers" objects
+# EOF allow shell to read all commands at the same time, this prevents me getting error "Access denied for user 'root'@'localhost' (using password: NO)"
+# since the alter user command changes the root password and would need me to authenticate again
